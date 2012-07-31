c76700f5d30d102a6e8292d9bcdb3d654739a2b4
Athikom Vorapattanapong <akom.was.here91@hotmail.com>
2012-07-30 18:45:52 -0700

    started HasSong Class

---
+++ HasSong.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+
+// for reading from the command line
+import java.io.*;
+
+
+
+public class HasSong{
+	
+	private int upc;
+	
+	private String title;
+	
+    private Statement stmt;
+    private ResultSet rs;
+    
+    public HasSong( int upc, String title){
+    	
+    	upc = this.upc;
+    	
+    }
+	
+}
