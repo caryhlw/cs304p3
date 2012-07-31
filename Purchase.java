70e7423de6d724a6c4b52bea02b4dd9b6ccafd57
Cary Wong <melstrom@gmail.com>
2012-07-30 23:40:18 -0700

    Added subtotal calculation

--- Purchase.java
+++ Purchase.java
@@ -4,52 +4,55 @@ import java.util.ArrayList;
 public class Purchase {
 
     private ArrayList purchaseItems;
+
     private int receiptId;
     private String date;
     private int cardnum;
     private int expiry;
     private String expectedDate;
     private String deliveredDate;
+    
+    private float subtotal;
+
     private Statement stmt;
     private ResultSet rs;
+    private Connection con;
 
-    public Purchase() {
+    public Purchase(Connection con) {
         //fetchReceiptID
         //getCurrentDate
+        this.con = con;
         ArrayList purchaseItems = new ArrayList();
     }
 
     public void addItem(int upc) {
         try
         {
-            Connection con = DriverManager.getConnection(
-                    "jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1521:ug",
-                    "username",
-                    "password");
-
-            PurchaseItem purchaseItem = new PurchaseItem(upc, receiptId);
+            Item Item = new Item(con, upc);
             stmt = con.createStatement();
             rs = stmt.executeQuery("SELECT quantity"
                     + "FROM PurchaseItem"
                     + "WHERE receiptId = " + receiptId + ", upc = " + upc + ";");
             if (rs.next() == false)
             {
-                if (checkStock(purchaseItem, 1) == true)
+                if (checkStock(Item, 1) == true)
                 stmt.executeUpdate("insert into PurchaseItem values"
                         + "(" + receiptId + "," + upc + ", 1);");
-                purchaseItems.add(purchaseItem);
+                purchaseItems.add(Item);
+            }
 
-            } else
+            else
             {
-                if (checkStock(purchaseItem, rs.getInt(1)))
+                if (checkStock(Item, rs.getInt(1)))
                 stmt.executeUpdate("Update PurchaseItem"
                         + "SET quantity = quantity + 1"
                         + "WHERE receiptId = " + receiptId + ", upc = " + upc + ";");
-                purchaseItems.add(purchaseItem);
-
+                purchaseItems.add(Item);
             }
 
-        } catch (SQLException ex)
+        }
+
+        catch (SQLException ex)
         {
             System.out.println("Message: " + ex.getMessage());
         }
@@ -79,11 +82,23 @@ public class Purchase {
         return deliveredDate;
     }
 
-    private boolean checkStock(PurchaseItem pi, int purchaseQuantity) {
+    private boolean checkStock(Item pi, int purchaseQuantity) {
         int stock = pi.getQuantity();
         if (stock > purchaseQuantity)
             return true;
         else
             return false;
     }
+
+    private void calculateSubtotal ()
+    {
+        float subtotal = 0;
+        Object purchaseItems[] = this.purchaseItems.toArray();
+        for (int i = 0; i < this.purchaseItems.size(); i++)
+        {
+            subtotal =+ ((Item) purchaseItems[i]).getSellPrice();
+        }
+        this.subtotal = subtotal;
+    }
+    
 }
