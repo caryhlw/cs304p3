ba3c806177b0c07a3b50740b8de055debe2260c1
Cary Wong <melstrom@gmail.com>
2012-07-31 00:01:04 -0700

    New constructor supports seacrching by id

--- Purchase.java
+++ Purchase.java
@@ -1,31 +1,46 @@
+
 import java.sql.*;
 import java.util.ArrayList;
 
-public class Purchase {
+public class Purchase
+{
 
     private ArrayList purchaseItems;
-
     private int receiptId;
     private String date;
     private int cardnum;
     private int expiry;
     private String expectedDate;
     private String deliveredDate;
-    
     private float subtotal;
-
     private Statement stmt;
     private ResultSet rs;
     private Connection con;
 
-    public Purchase(Connection con) {
+    public Purchase(Connection con)
+    {
         //fetchReceiptID
         //getCurrentDate
         this.con = con;
-        ArrayList purchaseItems = new ArrayList();
+        this.purchaseItems = new ArrayList();
     }
 
-    public void addItem(int upc) {
+    public Purchase(Connection con, int receiptId)
+    {
+        try
+        {
+            stmt = con.createStatement();
+            rs = stmt.executeQuery("SELECT receiptId, date, cardnum, expiry, expectedDate, deliveredDate"
+                    + "FROM Purchase"
+                    + "WHERE receiptId = " + receiptId + ";");
+        } catch (SQLException ex)
+        {
+            System.out.println("Message: " + ex.getMessage());
+        }
+    }
+
+    public void addItem(int upc)
+    {
         try
         {
             Item Item = new Item(con, upc);
@@ -36,69 +51,78 @@ public class Purchase {
             if (rs.next() == false)
             {
                 if (checkStock(Item, 1) == true)
-                stmt.executeUpdate("insert into PurchaseItem values"
-                        + "(" + receiptId + "," + upc + ", 1);");
+                {
+                    stmt.executeUpdate("insert into PurchaseItem values"
+                            + "(" + receiptId + "," + upc + ", 1);");
+                }
                 purchaseItems.add(Item);
-            }
-
-            else
+            } else
             {
                 if (checkStock(Item, rs.getInt(1)))
-                stmt.executeUpdate("Update PurchaseItem"
-                        + "SET quantity = quantity + 1"
-                        + "WHERE receiptId = " + receiptId + ", upc = " + upc + ";");
+                {
+                    stmt.executeUpdate("Update PurchaseItem"
+                            + "SET quantity = quantity + 1"
+                            + "WHERE receiptId = " + receiptId + ", upc = " + upc + ";");
+                }
                 purchaseItems.add(Item);
             }
 
-        }
-
-        catch (SQLException ex)
+        } catch (SQLException ex)
         {
             System.out.println("Message: " + ex.getMessage());
         }
     }
 
-    public int receiptId() {
+    public int receiptId()
+    {
         return receiptId;
     }
 
-    public String date() {
+    public String date()
+    {
         return date;
     }
 
-    public int cardnum() {
+    public int cardnum()
+    {
         return cardnum;
     }
 
-    public int expiry() {
+    public int expiry()
+    {
         return expiry;
     }
 
-    public String expectedDate() {
+    public String expectedDate()
+    {
         return expectedDate;
     }
 
-    public String deliveredDate() {
+    public String deliveredDate()
+    {
         return deliveredDate;
     }
 
-    private boolean checkStock(Item pi, int purchaseQuantity) {
+    private boolean checkStock(Item pi, int purchaseQuantity)
+    {
         int stock = pi.getQuantity();
         if (stock > purchaseQuantity)
+        {
             return true;
-        else
+        } else
+        {
             return false;
+        }
     }
 
-    private void calculateSubtotal ()
+    private void calculateSubtotal()
     {
         float subtotal = 0;
         Object purchaseItems[] = this.purchaseItems.toArray();
         for (int i = 0; i < this.purchaseItems.size(); i++)
         {
-            subtotal =+ ((Item) purchaseItems[i]).getSellPrice();
+            subtotal = +((Item) purchaseItems[i]).getSellPrice();
         }
         this.subtotal = subtotal;
     }
-    
 }
