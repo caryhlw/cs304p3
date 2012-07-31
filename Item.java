494840afd46a41040ce60f771eb7fa03ab072f37
Cary Wong <melstrom@gmail.com>
2012-07-30 22:44:43 -0700

    Redone constructors

--- Item.java
+++ Item.java
@@ -1,93 +1,122 @@
+
 import java.sql.*;
 
-public class Item {
+public class Item
+{
 
-	private int upc;
-	private String title;
-	private String type;
-	private String category;
-	private String company;
-	private int year;
-	private float sellPrice;
-	private int quantity;
-	private Statement stmt;
-	private ResultSet rs;
-	private Connection con;
-	
-	public Item (Connection con, int upc)
-	{
-	upc = this.upc;
-	
-		try
+    private int upc;
+    private String title;
+    private String type;
+    private String category;
+    private String company;
+    private int year;
+    private float sellPrice;
+    private int quantity;
+    private Statement stmt;
+    private ResultSet rs;
+
+    public Item(int upc, String title, String type, String category, String company, int year, float sellPrice)
+    {
+        this.upc = upc;
+        this.title = title;
+        this.type = type;
+        this.category = category;
+        this.company = company;
+        this.year = year;
+        this.sellPrice = sellPrice;
+    }
+
+    public Item(Connection con, int upc)
+    {
+        try
         {
-			Connection con = DriverManager.getConnection(
-                    "jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1521:ug",
-                    "username",
-                    "password");
             stmt = con.createStatement();
-            rs = stmt.executeQuery("SELECT title, type, category, company, year, sellPrice, quantity"
-                    + "FROM Item"
+            rs = stmt.executeQuery("SELECT upc, title, type, category, company, year, sellPrice, quantity"
+                    + "FROM item"
                     + "WHERE upc = " + upc + ";");
-            
             if (rs.next() == true)
             {
-              title = rs.getNString(1);
-			  type = rs.getNString(2);
-			  category = rs.getNString(3);
-			  company = rs.getNString(4);
-			  year = rs.getInt(5);
-			  sellPrice = rs.getFloat(6);
-			  quantity = rs.getInt(7);
+            this.upc = rs.getInt(1);
+            this.title = rs.getString(2);
+            this.type = rs.getString(3);
+            this.category = rs.getString(4);
+            this.company = rs.getString(5);
+            this.year = rs.getInt(6);
+            this.sellPrice = rs.getFloat(7);
+            this.quantity = rs.getInt(8);
             }
-            
-        
+            else
+                throw new SQLException ("UPC does not exist");
         }
-        
         catch (SQLException ex)
         {
             System.out.println("Message: " + ex.getMessage());
         }
     }
-	
-	private void addItem()
-	
-	    {
-        try
-        {
-                             stmt = con.createStatement();
-                             stmt.executeUpdate("afdsafdslkjsadl"); //insert new item
 
-        }
-                   catch (SQLException ex)
-        {
-            System.out.println("Message: " + ex.getMessage());
-        }
+    public int getUPC()
+    {
+        return upc;
+    }
 
-	
-	};
-int getUPC(){
-	return upc;
-	}
-String gettitle(){
-	return title;
-	}
-String gettype(){
-	return type;
-	}
-String getcategory(){
-	return category;
-	}
-String getcompany(){
-	return company;
-	}
-int getyear(){
-	return year;
-	}
-float getsellPrice(){
-	return sellPrice;
-	}
-int getquantity(){
-	return quantity;
-	}
-	
+    public String getTitle()
+    {
+        return title;
+
+
+
+
+    }
+
+    public String getType()
+    {
+        return type;
+
+
+
+
+    }
+
+    public String getCategory()
+    {
+        return category;
+
+
+
+
+    }
+
+    public String getCompany()
+    {
+        return company;
+
+
+
+
+    }
+
+    public int getYear()
+    {
+        return year;
+
+
+
+
+    }
+
+    public float getSellPrice()
+    {
+        return sellPrice;
+
+
+
+
+    }
+
+    public int getQuantity()
+    {
+        return quantity;
+
+
+    }
 }
