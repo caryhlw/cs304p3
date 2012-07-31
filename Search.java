2f57b97bee394a9c620d20dbba297da9659edec7
Cary Wong <melstrom@gmail.com>
2012-07-31 15:22:19 -0700

    Formatting

--- Search.java
+++ Search.java
@@ -1,158 +1,157 @@
+
 import java.sql.*;
 import java.util.ArrayList;
 
+public class Search
+{
 
-public class Search {
-	
-	private ArrayList <Item> retItem;
-	private String sTerm;
-	private Connection con;
-	
-	private Statement stmt;
-	private ResultSet rs;
-	private PreparedStatement  ps;
-	
-	public Search(Connection con){
-		retItem = new ArrayList<Item>(0);
-		this.con = con;
-		sTerm = null;
-		
-	}
-	
-	void searchTitle(){
-		try{
-			
-		ps = con.prepareStatement("SELECT upc "
-				+"FROM Item"
-				+"WHERE title = ?");
-		sTerm = in.readLine();
-		//sTerm = search Term
-		if (sTerm.length() == 0)
+    private ArrayList<Item> retItem;
+    private String sTerm;
+    private Connection con;
+    private Statement stmt;
+    private ResultSet rs;
+    private PreparedStatement ps;
+
+    public Search(Connection con)
+    {
+        retItem = new ArrayList<Item>(0);
+        this.con = con;
+        sTerm = null;
+
+    }
+
+    void searchTitle(String title)
+    {
+        try
         {
-	      ps.setString(1, null);
-	  }
-	  else
-	  {
-	      ps.setString(1, sTerm);
-	  }
-		rs = ps.executeQuery();
 
-		while( rs.next() ){
-			
-			retItem.add(new Item(con,rs.getInt(1)));
-		}
-		
-		  // commit work 
-		  con.commit();
+            ps = con.prepareStatement("SELECT upc "
+                    + "FROM Item"
+                    + "WHERE title = ?");
+            //sTerm = search Term
+            if (title.length() == 0)
+            {
+                ps.setString(1, null);
+            } else
+            {
+                ps.setString(1, sTerm);
+            }
+            rs = ps.executeQuery();
 
-		  ps.close();
-		}
-		
-		catch (SQLException ex)
-		{
-		    System.out.println("Message: " + ex.getMessage());
-		    try 
-		    {
-			// undo the insert
-			con.rollback();	
-		    }
-		    catch (SQLException ex2)
-		    {
-			System.out.println("Message: " + ex2.getMessage());
-			System.exit(-1);
-		    }
-		}
-	}
-	
-	
-	void searchCategory(){
-		try{
-		ps = con.prepareStatement("SELECT upc"
-				+"FROM Item"
-				+"WHERE category = ?");
-		sTerm = in.readLine();
-		//sTerm = search Term
-		if (sTerm.length() == 0)
+            while (rs.next())
+            {
+
+                retItem.add(new Item(con, rs.getInt(1)));
+            }
+
+            // commit work
+            con.commit();
+
+            ps.close();
+        } catch (SQLException ex)
         {
-	      ps.setString(1, null);
-	  }
-	  else
-	  {
-	      ps.setString(1, sTerm);
-	  }
-		rs = ps.executeQuery();
+            System.out.println("Message: " + ex.getMessage());
+            try
+            {
+                // undo the insert
+                con.rollback();
+            } catch (SQLException ex2)
+            {
+                System.out.println("Message: " + ex2.getMessage());
+                System.exit(-1);
+            }
+        }
+    }
 
-		while( rs.next() ){
-			
-			retItem.add(new Item (con,rs.getInt(1)));
-		}
-		
-		  // commit work 
-		  con.commit();
-
-		  ps.close();
-		}
-		
-		catch (SQLException ex)
-		{
-		    System.out.println("Message: " + ex.getMessage());
-		    try 
-		    {
-			// undo the insert
-			con.rollback();	
-		    }
-		    catch (SQLException ex2)
-		    {
-			System.out.println("Message: " + ex2.getMessage());
-			System.exit(-1);
-		    }
-		}
-	}
-	void searchLeadSinger(){
-		try{
-		ps = con.prepareStatement("SELECT upc"
-				+"FROM Item I, LeadingSinger L"
-				+"WHERE I.upc = L.upc AND L.name = ?");
-		sTerm = in.readLine();
-		//sTerm = search Term
-		if (sTerm.length() == 0)
+    void searchCategory()
+    {
+        try
         {
-	      ps.setString(1, null);
-	  }
-	  else
-	  {
-	      ps.setString(1, sTerm);
-	  }
-		rs = ps.executeQuery();
+            ps = con.prepareStatement("SELECT upc"
+                    + "FROM Item"
+                    + "WHERE category = ?");
+            sTerm = in.readLine();
+            //sTerm = search Term
+            if (sTerm.length() == 0)
+            {
+                ps.setString(1, null);
+            } else
+            {
+                ps.setString(1, sTerm);
+            }
+            rs = ps.executeQuery();
 
-		while( rs.next() ){
-			
-			retItem.add(new Item(con,rs.getInt(1)));
-		}
-		
+            while (rs.next())
+            {
 
-		  // commit work 
-		  con.commit();
+                retItem.add(new Item(con, rs.getInt(1)));
+            }
 
-		  ps.close();
-		}
-		
-		catch (SQLException ex)
-		{
-		    System.out.println("Message: " + ex.getMessage());
-		    try 
-		    {
-			// undo the insert
-			con.rollback();	
-		    }
-		    catch (SQLException ex2)
-		    {
-			System.out.println("Message: " + ex2.getMessage());
-			System.exit(-1);
-		    }
-		}
-	}
-	Item results(int index){
-		return retItem.get(index);
-	}
+            // commit work
+            con.commit();
+
+            ps.close();
+        } catch (SQLException ex)
+        {
+            System.out.println("Message: " + ex.getMessage());
+            try
+            {
+                // undo the insert
+                con.rollback();
+            } catch (SQLException ex2)
+            {
+                System.out.println("Message: " + ex2.getMessage());
+                System.exit(-1);
+            }
+        }
+    }
+
+    void searchLeadSinger()
+    {
+        try
+        {
+            ps = con.prepareStatement("SELECT upc"
+                    + "FROM Item I, LeadingSinger L"
+                    + "WHERE I.upc = L.upc AND L.name = ?");
+            sTerm = in.readLine();
+            //sTerm = search Term
+            if (sTerm.length() == 0)
+            {
+                ps.setString(1, null);
+            } else
+            {
+                ps.setString(1, sTerm);
+            }
+            rs = ps.executeQuery();
+
+            while (rs.next())
+            {
+
+                retItem.add(new Item(con, rs.getInt(1)));
+            }
+
+
+            // commit work
+            con.commit();
+
+            ps.close();
+        } catch (SQLException ex)
+        {
+            System.out.println("Message: " + ex.getMessage());
+            try
+            {
+                // undo the insert
+                con.rollback();
+            } catch (SQLException ex2)
+            {
+                System.out.println("Message: " + ex2.getMessage());
+                System.exit(-1);
+            }
+        }
+    }
+
+    Item results(int index)
+    {
+        return retItem.get(index);
+    }
 }
