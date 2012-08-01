9c248c4aae2b8cdc454311ad76ac2555d847a7d9
Cary Wong <melstrom@gmail.com>
2012-07-31 19:15:23 -0700

    Saving work for Akom

--- Shipment.java
+++ Shipment.java
@@ -1,25 +1,53 @@
+
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
-public class Shipment {
+public class Shipment
+{
+    private ArrayList shipItems;
+    private int sid;
+    private String supName;
+    private Date date;
+    private Statement stmt;
+    private ResultSet rs;
+    private Connection con;
+    private PreparedStatement ps;
 
-	private int sid;
-	private String supName;
-	private String sdate;
-	private Statement stmt;
-	private ResultSet rs;
-	
-	public Shipment (int sid)
-	{
-	this.sid = sid;
-	}
+    public Shipment(Connection con, String supName)
+    {
+        this.con = con;
+        this.supName = supName;
+        this.shipItems = new ArrayList();
+        this.date = new Date();
+        long t = date.getTime();
+        java.sql.Date dt = new java.sql.Date(t);
+        try
+        {
+            ps = con.prepareStatement("INSERT INTO Shipment VALUES"
+                    + "(shipment_counter.next, ?, ?)");
+            ps.setString(1, this.supName);
+            ps.setDate(2, dt);
+            
+        } catch (SQLException ex)
+        {
+            System.out.println("Message: " + ex.getMessage());
+        }
+    }
 
-	int getSid(){
-		return sid;
-	}
-	String getSupName(){
-		return supName;
-	}
-	String getSDate(){
-		return sdate;
-	}
+    int getSid()
+    {
+        return sid;
+    }
+
+    String getSupName()
+    {
+        return supName;
+    }
+
+    String getSDate()
+    {
+        return sdate;
+    }
 }
