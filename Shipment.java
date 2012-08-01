774bbac831a4a6510d75e758bda9372e7c346563
Athikom Vorapattanapong <akom.was.here91@hotmail.com>
2012-07-31 19:17:19 -0700

    before pull

--- Shipment.java
+++ Shipment.java
@@ -5,14 +5,25 @@ public class Shipment {
 	private int sid;
 	private String supName;
 	private String sdate;
+	private ArrayList <int> itemList;
+	private Connection con;
 	private Statement stmt;
 	private ResultSet rs;
 	
-	public Shipment (int sid)
+	public Shipment (Connection con, int sid, String supName, String sdate, int firstItemUPC)
 	{
-	this.sid = sid;
+		this.con = con;
+		this.sid = sid;
+		this.supName = supName;
+		this.sdate = sdate;
+		itemList = new ArrayList<int>(1);
+		itemList.set(0, firstItemUPC);
 	}
-
+	
+	public void addItem( int UPC ){
+		con.prepareStatement(sql);
+	}
+	
 	int getSid(){
 		return sid;
 	}
