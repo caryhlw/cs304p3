50ca3f97c559e230dd98e5fdde181122893256ad
blau2 <brandonlau2@gmail.com>
2012-08-01 11:52:10 -0700

    working as intended

--- Customer.java
+++ Customer.java
@@ -4,86 +4,80 @@ import java.sql.*;
 public class Customer
 {
 
-    private int customerId;
+    private String customerId;
     private String password;
     private String name;
     private String address;
     private String phone;
     private Connection con;
     private Statement stmt;
-    private PreparedStatement ps = null;
+    private PreparedStatement ps;
 
-    public Customer(Connection con, int customerId, String password, String name, String address, String phone)
+    public Customer(Connection con, String customerId, String password, String name, String address, String phone)
     {
-        try
-        {
+       
             this.con = con;
             if (!existingCustomer(customerId))
             {
-                //If not found, add to DB
-                String addCust = "insert into Customer values (?, ?, ?, ?, ?)";
-                ps = con.prepareStatement(addCust);
-                ps.setInt(1, customerId);
-                ps.setString(2, password);
-                ps.setString(3, name);
-                ps.setString(4, phone);
-                ps.executeUpdate();
-
+            	
                 setCustomerId(customerId);
                 setPassword(password);
                 setName(name);
                 setAddress(address);
                 setPhone(phone);
+                
+                addCustomer();
+                
+            } else {
+            	System.out.println("User already exists in the database\n");
             }
 
-        } catch (SQLException ex)
-        {
-            System.out.println("Message: " + ex.getMessage());
-        }
     }
 
-    public Customer(int customerId, String password)
+    public Customer(Connection con, String customerId, String password)
     // Searches for the Customer in the database, given his customerId and password. If the Customer exists, and
     // the password matches the customerId, create a new Customer object with the information stored in the database.
     // Otherwise, throw a UserException.
     {
     try {
+    	this.con = con;
         if (existingCustomer(customerId))
         {
             String getPass = "select password from Customer where cid = ?";
             ps = con.prepareStatement(getPass);
-            ps.setInt(1, customerId);
+            ps.setString(1, customerId);
             ResultSet rs = ps.executeQuery();
+            rs.next();
             String tempPass = rs.getString(1);
-            if (tempPass != password)
+            System.out.println(tempPass);
+            if (!tempPass.equals(password))
             {
-                throw new Exception("Incorrect password");
+                System.out.println("Incorrect password");
             } else
             {
                 this.customerId = customerId;
                 password = tempPass;
                 String getUser = "select name, address, phone from Customer where cid = ?";
                 ps = con.prepareStatement(getUser);
-                ps.setInt(1, customerId);
+                ps.setString(1, customerId);
                 rs = ps.executeQuery();
+                rs.next();
                 name = rs.getString(1);
                 address = rs.getString(2);
                 phone = rs.getString(3);
             }
         } else
         {
-            throw new Exception("User does not exist");
+            System.out.println("User does not exist");
         }
     	} catch (SQLException e) {
-    		System.out.println("Message: " + e.getMessage());
+    		System.out.println("Message: SQLException " + e.getMessage());
     		
-    	} catch (Exception ex) {
-    		System.out.println("Message: " + ex.getMessage());
-    	}
+    	} 
         //Find customer and see if password matches
     }
 
-    private void setCustomerId(int cid)
+    private void setCustomerId(String cid)
     {
         customerId = cid;
     }
@@ -114,10 +108,12 @@ public class Customer
         {
             String addCust = "insert into Customer values (?, ?, ?, ?, ?)";
         	ps = con.prepareStatement(addCust);
-            ps.setInt(1, customerId);
+            ps.setString(1, customerId);
             ps.setString(2, password);
             ps.setString(3, name);
-            ps.setString(4, phone);
+            ps.setString(4, address);
+            ps.setString(5, phone);
+
             ps.executeUpdate();
 
             ps.close();
@@ -132,27 +128,28 @@ public class Customer
 
     }
 
-    private boolean existingCustomer(int cid)
+    private boolean existingCustomer(String cid)
     {
         // returns true if cid already exists in the database; otherwise returns false
         try
         {
-            String searchCust = "select count(*) from Customer C"
-                    + "where C.cid = ?";
+            String searchCust = "select count(*) from Customer C where C.cid = ?";
             ps = con.prepareStatement(searchCust);
-            ps.setString(1, searchCust);
+            ps.setString(1, cid);
             ResultSet rs = ps.executeQuery();
+            rs.next();
             int rowCount = rs.getInt(1);
-            if (rowCount == 1)
+            if (rowCount != 0)
             {
                 return true;
             } else
             {
+            	//System.out.println("User does not exist \n");
                 return false;
             }
         } catch (SQLException ex)
         {
-            System.out.println("Message: " + ex.getMessage());
+            System.out.println("cid not found. Message: " + ex.getMessage());
             return true;
         }
 
