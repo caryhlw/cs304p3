4eff2ce7aa955ea64226c2390a2db05859d82a17
Cary Wong <melstrom@gmail.com>
2012-08-07 20:21:05 -0700

    Constructor now updates sid Add item updates db - previously only updates class

--- Shipment.java
+++ Shipment.java
@@ -6,24 +6,20 @@ import java.text.SimpleDateFormat;
 
 public class Shipment
 {
+    private int sid;
+    private String supName;
+    private Date date;
+    private ArrayList<Item> shipItems;
+    private Connection con;
+    private ResultSet rs;
+    private PreparedStatement ps;
 
-	private int sid;
-	private String supName;
-	private Date date;
-	private ArrayList<Item>shipItems;
-	private Connection con;
-	private Statement stmt;
-	private ResultSet rs;
-	private PreparedStatement ps;
-	
-	public Shipment (Connection con, int sid, String supName, int firstItemUPC)
-	{
-		this.con = con;
-		this.sid = sid;
-		this.supName = supName.toLowerCase();
-		shipItems = new ArrayList<Item>(0);
-		shipItems.add(new Item(con, firstItemUPC));
-		this.date = new Date();
+    public Shipment(Connection con, String supName)
+    {
+        this.con = con;
+        this.supName = supName.toLowerCase();
+        shipItems = new ArrayList<Item>(0);
+        this.date = new Date();
         long t = date.getTime();
         java.sql.Date dt = new java.sql.Date(t);
         try
@@ -32,58 +28,65 @@ public class Shipment
                     + "(shipment_counter.next, ?, ?)");
             ps.setString(1, this.supName);
             ps.setDate(2, dt);
-            
+
         } catch (SQLException ex)
         {
             System.out.println("Message: " + ex.getMessage());
         }
-	}
-	
-	public void addItem( int UPC ){
+    }
 
-            shipItems.add(new Item(con, UPC));
-       
-	}
-	
-	public void setSupName(String supName){
-		try{
-		this.supName = supName.toLowerCase();
-		ps = con.prepareStatement("Update Into Shipment set supName = ?"
-				+ "WHERE sid = "+ sid);
-		ps.setString(1, supName.toLowerCase());
-		ps.executeUpdate();
-		con.commit();
-		ps.close();
-		con.close();
-	}catch(SQLException ex) {
-        System.out.println("Message: " + ex.getMessage());
+    public void addItem(int UPC)
+    {
+        shipItems.add(new Item(con, UPC));
+    }
+
+    public void setSupName(String supName)
+    {
         try
         {
-            // undo the insert
-            con.rollback();
-        } catch (SQLException ex2)
+            this.supName = supName.toLowerCase();
+            ps = con.prepareStatement("Update Into Shipment set supName = ?"
+                    + "WHERE sid = " + sid);
+            ps.setString(1, supName.toLowerCase());
+            ps.executeUpdate();
+            con.commit();
+            ps.close();
+            con.close();
+        } catch (SQLException ex)
         {
-            System.out.println("Message: " + ex2.getMessage());
-            System.exit(-1);
+            System.out.println("Message: " + ex.getMessage());
+            try
+            {
+                // undo the insert
+                con.rollback();
+            } catch (SQLException ex2)
+            {
+                System.out.println("Message: " + ex2.getMessage());
+                System.exit(-1);
+            }
         }
-    	}
-		
-	}
-	public float getPrice( int itemUPC){
-		for( int i = 0; i < shipItems.size(); i++){
-			if( shipItems.get(i).getUPC() == itemUPC ){
-				return shipItems.get(i).getSellPrice();
-			}
-			
-		}
-		System.out.println("Item doesn't exist in this shipment");
-		return 0.0f;
-	}
-	
-	public int getSid()
+
+    }
+
+    public float getPrice(int itemUPC)
+    {
+        for (int i = 0; i < shipItems.size(); i++)
+        {
+            if (shipItems.get(i).getUPC() == itemUPC)
+            {
+                return shipItems.get(i).getSellPrice();
+            }
+
+        }
+        System.out.println("Item doesn't exist in this shipment");
+        return 0.0f;
+    }
+
+    public int getSid()
     {
         return sid;
     }
+
     public String getSupName()
     {
         return supName;
@@ -94,4 +97,3 @@ public class Shipment
         return date;
     }
 }
-
